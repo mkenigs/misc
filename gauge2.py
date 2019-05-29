63cff17d2e13145a138c09b6b09c024e70be2687
mkenigs <matthewkenigsberg@gmail.com>
2019-05-29 10:47:24 -0700

    made Gauge an object, added set() method
    example docstring
    
    tweaked padding, renamed start and end

--- gauge2.py
+++ gauge2.py
@@ -1,96 +1,104 @@
 #!/usr/bin/env python
 """
-Speed-Gauge for matplotlib
-See http://nbviewer.ipython.org/gist/nicolasfauchereau/794df533eca594565ab3
-Adapted to be more typical ratio display.
+Gauge for matplotlib
+Modified from http://nbviewer.ipython.org/gist/nicolasfauchereau/794df533eca594565ab3, https://github.com/martindurant/misc/blob/master/gauge.py
+Added set() so can be updated dynamically
 """
 
 from matplotlib import cm
 from matplotlib import pyplot as plt
-import numpy as np
+from matplotlib.patches import Circle, Wedge
+import math
 
-from matplotlib.patches import Circle, Wedge, Rectangle
+class Gauge():
+    """Example usage in Juypter Notebook:
 
-
-def degree_range(n):
-    start = np.linspace(0, 180, n+1, endpoint=True)[0:-1]
-    end = np.linspace(0, 180, n+1, endpoint=True)[1::]
-    mid_points = start + ((end-start)/2.)
-    return np.c_[start, end], mid_points
-
-
-def rot_text(ang):
-    rotation = np.degrees(np.radians(ang) * np.pi / np.pi - np.radians(90))
-    return rotation
-
-
-def gauge(colors='hot', title='', value=0.5, labs=['0%', -1, '100%']):
-    """
-Gauge display of a single value against a range.
-
-Parameters
-----------
-
-hot : string
-    matplotlib colour map specification
-
-title : string
-    To put at the base of the gauge. If falsy, no label area.
-
-value : float
-    Fractional value to plot, 0 for no colour, 1 for whole bar
-
-labs : 3-element list of tuple
-    Label the start, arrow and end of the gauge with these. If the middle one
-    is -1, replace with percentage of value.
+    import time
+    from IPython import display
+    g=Gauge(0, 100, 0, tickStep=10, title="My Gauge")
+    for i in range(6):
+        display.display(g.fig)
+        g.set(i*20)
+        display.clear_output(wait=True)
+        time.sleep(.15)
     """
 
-    N = 100  # number of elements
+    def valToDeg(self, value):
+        return self.angleStart+value/float(self.valEnd-self.valStart)*(self.angleEnd-self.angleStart)
 
-    # if colors is a colormap
-    cmap = cm.get_cmap(colors, N)
-    cmap = cmap(np.arange(N))
-    colors = cmap[::-1, :].tolist()
+    def updatedArrow(self):
+        theta = self.valToDeg(self.value)
+        return self.ax.arrow(*self.center, self.arrowLength * math.cos(math.radians(theta)), self.arrowLength * math.sin(math.radians(theta)), width=0.04, head_width=0.09, head_length=0.1, fc='k', ec='k')
 
-    fig, ax = plt.subplots()
-    ang_range, mid_points = degree_range(N)
-    pos = mid_points[int(N*(1-value))]
+    def __init__(self, valStart, valEnd, value, colors='hot', title=None, nWedges=30, angleStart=180, angleEnd=0, tickStep=None):
 
-    patches = []
-    for ang, c in zip(ang_range, colors):
-        # sectors
-        # patches.append(Wedge((0., 0.), .4, *ang, facecolor='w', lw=2))
-        # arcs
-        if sum(ang) < pos*2:
-            c = 'w'
-        patches.append(Wedge((0., 0.), .4, *ang, width=0.10,
-                             facecolor=c, lw=2, alpha=0.5, edgecolor='None'))
+        # Only need to keep values used by set()
+        self.value = value
+        self.angleStart = angleStart
+        self.angleEnd = angleEnd
+        self.valStart = valStart
+        self.valEnd = valEnd
 
-    for mid, lab in zip([180, pos, 0], labs):
-        if lab == -1:
-            lab = "{:.1%}".format(value)
-        if lab:
-            ax.text(0.41 * np.cos(np.radians(mid)), 0.41 * np.sin(np.radians(mid)),
-                    lab, ha='center', va='center', fontsize=14, fontweight='bold',
-                    rotation=rot_text(mid))
+        # Constants
+        radius = 1
+        width = .4
+        labelPad = .06
+        self.center = (0, 0)
+        self.arrowLength = .37
 
-    [ax.add_patch(p) for p in patches]
+        self.fig, self.ax = plt.subplots()
 
-    if title:
-        r = Rectangle((-0.4, -0.1), 0.8, 0.1, facecolor='w', lw=2)
-        ax.add_patch(r)
-        ax.text(0, -0.05, title, horizontalalignment='center',
-                verticalalignment='center', fontsize=22, fontweight='bold')
+        # Wedges
+        # if colors is a colormap
+        cmap = cm.get_cmap(colors, nWedges)  # could throw
+        wedges = []
+        for i in range(nWedges):
+            theta0 = self.angleStart+i/float(nWedges)*(self.angleEnd-self.angleStart)
+            theta1 = self.angleStart+(i+1)/float(nWedges)*(self.angleEnd-self.angleStart)
+            # always want smaller theta first
+            if theta1 < theta0:
+                theta0, theta1 = theta1, theta0
 
-    ax.arrow(0, 0, 0.225 * np.cos(np.radians(pos)), 0.225 *
-             np.sin(np.radians(pos)), width=0.04, head_width=0.09,
-             head_length=0.1, fc='k', ec='k')
+            wedges.append(Wedge(self.center, radius, theta0, theta1,
+                                width=width, facecolor=cmap(i), edgecolor=cmap(i)))
 
-    ax.add_patch(Circle((0, 0), radius=0.02, facecolor='k'))
-    ax.add_patch(Circle((0, 0), radius=0.01, facecolor='w', zorder=11))
+        [self.ax.add_patch(p) for p in wedges]
 
-    ax.set_frame_on(False)
-    ax.axes.set_xticks([])
-    ax.axes.set_yticks([])
-    ax.axis('equal')
-    plt.tight_layout()
+        # Labels
+        if tickStep:
+            numTicks = 1 + \
+                math.floor((self.valEnd-self.valStart)/float(tickStep))
+            for i in range(numTicks):
+
+                theta = self.angleStart+i*tickStep * \
+                    (self.angleEnd-self.angleStart)/(self.valEnd-self.valStart)
+
+                x = (radius+labelPad)*math.cos(math.radians(theta))
+                y = (radius+labelPad)*math.sin(math.radians(theta))
+                text = self.valStart+i*tickStep
+                self.ax.text(x, y, text, ha='center', va='center',
+                             fontsize=14, fontweight='bold', rotation=theta-90)  # rotate text -90 so baseline faces the gauge
+
+        # Title
+        if title:
+            self.ax.text(0, -0.14, title, horizontalalignment='center',
+                         verticalalignment='center', fontsize=22, fontweight='bold')
+
+        # Arrow
+        self.arrow = self.updatedArrow()
+        self.ax.add_patch(Circle(self.center, radius=0.02, facecolor='k'))
+        self.ax.add_patch(
+            Circle(self.center, radius=0.01, facecolor='w', zorder=11))
+
+        # Hide axes
+        self.ax.set_frame_on(False)
+        self.ax.axes.set_xticks([])
+        self.ax.axes.set_yticks([])
+
+        # makes scaling square
+        self.ax.axis('equal')
+
+    def set(self, value):
+        self.value = value
+        self.arrow.remove()
+        self.arrow = self.updatedArrow()
